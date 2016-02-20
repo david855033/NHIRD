ab1c0ab2e3d6b0262c480c4eb5d5abae6cda0b59
david855033 <david855033@gmail.com>
2016-02-21 07:41:54 +0800

    doing dependent obj

--- NHIRD/GetOrder/window_GetOrder.xaml.cs
+++ NHIRD/GetOrder/window_GetOrder.xaml.cs
@@ -232,5 +232,7 @@ namespace NHIRD
         {
 
         }
+
+   
     }
 }
--- NHIRD/StringList.xaml.cs
+++ NHIRD/StringList.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,13 +25,25 @@ namespace NHIRD
         {
             InitializeComponent();
         }
-        public List<string> currentList = new List<string>();
+        public static readonly DependencyProperty ListProperty =
+        DependencyProperty.Register("_currentList", typeof(ObservableCollection<string>),
+        typeof(StringList), new PropertyMetadata(""));
+
+        public List<string> currentList
+        {
+            get { return (List<string>)GetValue(ListProperty); }
+            set { SetValue(ListProperty, value); }
+        }
+
+        public event Propert
+
+        ObservableCollection<string> _currentList = new ObservableCollection<string>();
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
             var stringtoAdd = Tx_input.Text.Replace("_", " ");
-            if (stringtoAdd.Length > 0 && !currentList.Any(x => x == stringtoAdd))
+            if (stringtoAdd.Length > 0 && !_currentList.Any(x => x == stringtoAdd))
             {
-                currentList.Add(stringtoAdd);
+                _currentList.Add(stringtoAdd);
                 Tx_input.Text = "";
                 refresh();
             }
@@ -38,10 +51,10 @@ namespace NHIRD
         private void ButtonEdt_Click(object sender, RoutedEventArgs e)
         {
             var stringtoAdd = Tx_input.Text.Replace("_", " ");
-            if (stringtoAdd.Length > 0 && Lv_StringList.SelectedItem != null && !currentList.Any(x => x == stringtoAdd))
+            if (stringtoAdd.Length > 0 && Lv_StringList.SelectedItem != null && !_currentList.Any(x => x == stringtoAdd))
             {
-                currentList.RemoveAt(Lv_StringList.SelectedIndex);
-                currentList.Insert(Lv_StringList.SelectedIndex, stringtoAdd);
+                _currentList.RemoveAt(Lv_StringList.SelectedIndex);
+                _currentList.Insert(Lv_StringList.SelectedIndex, stringtoAdd);
                 refresh();
             }
         }
@@ -51,28 +64,50 @@ namespace NHIRD
         {
             if (Lv_StringList.SelectedItem != null)
             {
-                currentList.RemoveAt(Lv_StringList.SelectedIndex);
+                _currentList.RemoveAt(Lv_StringList.SelectedIndex);
                 refresh();
             }
         }
 
         private void ButtonClr_Click(object sender, RoutedEventArgs e)
         {
-
+            _currentList.Clear();
+            refresh();
         }
 
         private void ButtonLoad_Click(object sender, RoutedEventArgs e)
         {
-
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "txt files (*.txt)|*.txt|All files(*.*)|*.*";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (var sr = new System.IO.StreamReader(dialog.FileName, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var stringtoAdd = sr.ReadLine().Replace("_", " ");
+                        if (stringtoAdd.Length > 0 && stringtoAdd.Length <= 12 && !_currentList.Any(x => x == stringtoAdd))
+                        {
+                            _currentList.Add(stringtoAdd);
+                            refresh();
+                        }
+                    }
+                }
+            }
         }
 
         void refresh()
         {
             Lv_StringList.Items.Clear();
-            foreach (var s in currentList)
+            foreach (var s in _currentList)
             {
                 Lv_StringList.Items.Add(s.Replace(" ", "_"));
             }
+
+            Cb_EnabaleCriteria.IsChecked = _currentList.Count > 0 ? true : false;
         }
     }
 }
