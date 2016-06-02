5992f459e1856d6c5fe2030063bdaa608edca4a6
david855033 <david855033@gmail.com>
2016-06-02 17:34:44 +0800

    doing ASI

--- NHIRD/WPF Forms/AgeSpecificIncidence/AgeSpecificIncidenceCalculator.cs
+++ NHIRD/WPF Forms/AgeSpecificIncidence/AgeSpecificIncidenceCalculator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace NHIRD
 {
     class AgeSpecificIncidenceCalculator
     {
         IEnumerable<MatchOfPatiendBasedDataAndStandarizedID> matchList;
+        List<string> EventNames;
+        AgeSpecificIncidenceTable[] ASITables;
         public AgeSpecificIncidenceCalculator(IEnumerable<MatchOfPatiendBasedDataAndStandarizedID> matchList)
         {
             this.matchList = matchList;
@@ -18,17 +20,67 @@ namespace NHIRD
         {
             System.Windows.MessageBox.Show("calculation ASI");
 
+            analyzeTitleOfPatientBasedDataAndGetEventNames(matchList.First().patientBasedData.path);
+            initializeAgeSpecificIncidenceTable();
+            foreach (var currentMatch in matchList)
+            {
+                CalculateOneMatch(currentMatch);
+            } 
 
-
-            // read PBD -> 先從title分析group list後 初始化List<string> eventNames,
-            // 再產生List<patient>(含ID/性別/Event[](field: hasThisEvent, eventAge))，再讀取每個病人資料
+            // read PBD -> 先從title分析group list後 初始化List<string> eventNames, OK
             // 初始化ASI table, 欄位: Age  男-人年 -- 女-人年 - 男EventCount -  女EventCount , 以此做數個陣列(每個eventname用一個table)
-            // 逐筆讀取Standarize ID, 比對有無出現在PBD裡面，然後增加所有ASI table的人年 以及對應的ASI table的evnetcount數量
-           
+            
+            // 再產生List<patient>(含ID/性別/Event[](field: hasThisEvent, eventAge))，再讀取每個病人資料
+            
+            // 逐筆讀取Standarize ID, 比對有無出現在PBD裡面，然後增加所有ASI table的人年 以及對應的ASI table的evnetcount數量           
             // 對每個match重複以上動作
-            // 結算男生/女生的ASI
+            // 輸出ASI
         }
 
-        
+        private void analyzeTitleOfPatientBasedDataAndGetEventNames(string anyPatientBasedDataPath)
+        {
+            EventNames = new List<string>();
+            using (var sr = new StreamReader(anyPatientBasedDataPath, Encoding.Default))
+            {
+                string title = sr.ReadLine();
+                string[] split = title.Split('\t');
+                var query = from q in split where q.IndexOf("-最早年紀") >= 0 select q;
+                foreach (var s in query)
+                {
+                    EventNames.Add(s.Replace("-最早年紀",""));
+                }
+            }
+        }
+        private void initializeAgeSpecificIncidenceTable()
+        {
+            ASITables = new AgeSpecificIncidenceTable[EventNames.Count];
+            for (int i = 0; i < ASITables.Count(); i++)
+            {
+                ASITables[i] = AgeSpecificIncidenceTable.getNewAgeSpecificIncidenceTable(EventNames[i]);
+            }
+        }
+
+
+        private void CalculateOneMatch(MatchOfPatiendBasedDataAndStandarizedID currentMatch)
+        {
+            readPBD(currentMatch.patientBasedData.path);
+            readSID(currentMatch.standarizedID.path);
+        }
+        private void readPBD(string filePath)
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+                string title = sr.ReadLine();
+                while (!sr.EndOfStream())
+                {
+
+                }
+            }
+        }
+        private void readSID(string filePath)
+        {
+
+        }
+
     }
 }
---
+++ NHIRD/WPF Forms/AgeSpecificIncidence/AgeSpecificIncidenceTable.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHIRD
+{
+    class AgeSpecificIncidenceTable
+    {
+        string tableName;
+
+        AgeSpecificIncidenceGroup[,] table =
+            new AgeSpecificIncidenceGroup[100, 2];
+
+        private AgeSpecificIncidenceTable(string tableName)
+        {
+            this.tableName = tableName;
+        }
+
+        static public AgeSpecificIncidenceTable getNewAgeSpecificIncidenceTable(string tableName)
+        {
+            return new AgeSpecificIncidenceTable(tableName);
+        }
+
+        public void addPatientYear(Gender gender, double startAge, double endAge)
+        {
+            int genderInt = gender == Gender.Male ? 1 : 0;
+            int startAgeInt = Convert.ToInt32(Math.Truncate(startAge));
+            int endAgeInt = Convert.ToInt32(Math.Truncate(endAge));
+            if (startAge > startAgeInt)
+            {
+                double startResidual = (startAgeInt + 1) - startAge;
+                table[startAgeInt, genderInt].patientYear += startResidual;
+                startAgeInt++;
+            }
+            if (endAgeInt < 100)
+            {
+                if (endAge > endAgeInt)
+                {
+                    double endResidual = endAge - endAgeInt;
+                    table[endAgeInt, genderInt].patientYear += endResidual;
+                }
+            }
+            else
+            {
+                endAgeInt = 100;
+            }
+
+            for (int i = startAgeInt; i < endAgeInt; i++)
+            {
+                table[i, genderInt].patientYear++;
+            }
+        }
+        public void addEvent(Gender gender, double eventAge)
+        {
+            int genderInt = gender == Gender.Male ? 1 : 0;
+            int eventAgeInt = Convert.ToInt32(Math.Truncate(eventAge));
+            table[eventAgeInt, genderInt].eventCount++;
+        }
+        public string getResult()
+        {
+            StringBuilder result = new StringBuilder();
+            string title1 = "" + "\tFemale\t\t" + "\tMale\t\t" + "\tTotal\t\t";
+            string title2 = "Age Group" + "\tPatient Year\tEvent Count\tAge Specific Incidence"
+                                        + "\tPatient Year\tEvent Count\tAge Specific Incidence"
+                                        + "\tPatient Year\tEvent Count\tAge Specific Incidence";
+            result.AppendLine(title1);
+            result.AppendLine(title2);
+
+            for (int i = 0; i < 100; i++)
+            {
+                result.AppendLine(
+                    i + "\t"
+                    + table[i, 0].patientYear.Round(1) + "\t"
+                    + table[i, 0].eventCount + "\t"
+                    + ((double)table[i, 0].eventCount / table[i, 0].patientYear * 100).Round(1) + "%\t"
+                    
+                    + table[i, 1].patientYear.Round(1) + "\t"
+                    + table[i, 1].eventCount + "\t"
+                    + ((double)table[i, 1].eventCount / table[i, 1].patientYear * 100).Round(1) + "%\t"
+                    
+                    + (table[i, 0].patientYear+ table[i, 1].patientYear).Round(1) + "\t"
+                    + (table[i, 0].eventCount+ table[i, 1].eventCount) + "\t"
+                    + ((double)(table[i, 0].eventCount+ table[i, 1].eventCount) / 
+                    (table[i, 0].patientYear+ table[i, 1].patientYear) * 100).Round(1) + "%\t"
+                    
+                    );
+            }
+
+            return result.ToString();
+        }
+
+    }
+
+    public enum Gender
+    {
+        Female, Male
+    }
+
+    class AgeSpecificIncidenceGroup
+    {
+        public double patientYear;
+        public int eventCount;
+    }
+}
